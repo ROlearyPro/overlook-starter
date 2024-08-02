84e2338faa134683dd1793204712bfe9552369db
ROlearyPro <rolearyprogramming@gmail.com>
2024-08-01 20:01:58 -0600

    functionality/ Added in search functionality for rooms, laid groundwork for ability to post, have filter set up on backend, though not implemented yet.

--- src/domUpdater.js
+++ src/domUpdater.js
@@ -1,18 +1,17 @@
 import { customerPromise, currentCustomer, customerList, randomCustomer, postBooking } from './apiCaller.js';
-import { setDataVals, receivedRooms, recievedCustomers, recievedBookings, receivedCustomers, currentBookings, hasSpent } from './functionCalls.js';
+import { setDataVals, receivedRooms, recievedCustomers, recievedBookings, receivedCustomers, currentBookings, hasSpent, findFreeRooms, currentFilter, currentDateSelection } from './functionCalls.js';
 const testArea = document.querySelector('.test-zone');
 const userNameArea = document.querySelector('.username-display');
 const priceArea = document.querySelector('.price-zone');
+const searchRoomsButton = document.querySelector('.search-button');
+const searchResults = document.querySelector('.search-results');
 let testArray = [];
 
-
 Promise.all([customerPromise]).then((values) => { randomCustomer(values) })
-// .then((values)=> console.log(values)).then();
-
 
 async function testFunction1() {
     await setDataVals();
-    
+
     var bookingDisplayData = await currentBookings();
     console.log(bookingDisplayData)
     bookingDisplayData.forEach(element => {
@@ -27,17 +26,57 @@ async function testFunction1() {
 }
 async function setUserName() {
     await setDataVals();
-    userNameArea.innerHTML= `${currentCustomer.name}`;
+    userNameArea.innerHTML = `${currentCustomer.name}`;
     console.log(recievedBookings);
 }
 
-async function tester (){
+async function tester() {
     await setDataVals();
-    priceArea.innerHTML+=`${await hasSpent(currentCustomer.id)} spent on rooms so far.`;
+    priceArea.innerHTML += `${await hasSpent(currentCustomer.id)} spent on rooms so far.`;
     console.log(currentCustomer)
 }
 
-// Todo: implement total amount spent on rooms
+async function userInput() {
+    const input = document.getElementById('searchInput').value;
+    var freeRooms;
+    freeRooms = await findFreeRooms(input, currentFilter);
+    currentDateSelection = input;
+    var buttonArray = []
+    if (freeRooms === null) {
+        alert('We\'re so sorry! We currently do not have rooms available for that date. You might have better luck trying for a different day?');
+    } else {
+        for (var i = 0; i < freeRooms.length; i++) {
+            searchResults.innerHTML += `<div class = "result-class container-for-results-${freeRooms[i].number}"> The ${freeRooms[i].roomType} with room number ${freeRooms[i].number} is free on ${input}. 
+            <button class="book-button-${freeRooms[i].number}" id="book-${freeRooms[i].number}">Book</button>
+            </div> `;
+
+        }
+        for(var i = 0; i < freeRooms.length; i++){
+            buttonArray[i] = document.querySelector(`.book-button-${freeRooms[i].number}`);
+            buttonArray[i].roomNum = freeRooms[i].number;
+            buttonArray[i].addEventListener('click', bookRoom);
+            
+        }
+        console.log(buttonArray)
+    };       
+
+    console.log(buttonArray);
+};
+
+
+function bookRoom () {
+    console.log(this);
+    var numHolder = this.roomNum;
+    console.log("booked!");
+    console.log(currentDateSelection);
+    console.log(currentCustomer.name);
+    console.log(currentCustomer.id);
+    console.log(numHolder)
+    // TODO: actually post the booking, we can already access all the needed data.
+}
+
+
+searchRoomsButton.addEventListener('click', userInput);
 
 testFunction1();
 setUserName();
--- src/functionCalls.js
+++ src/functionCalls.js
@@ -1,18 +1,21 @@
 import { bookingsPromise, customerPromise, roomsPromise, handleBookings, handleRooms, handleCustomers, bookingData, roomData, customerList, currentCustomer } from "./apiCaller";
-Promise.all([bookingsPromise]).then((values)=> {handleBookings(values)});
-Promise.all([customerPromise]).then((values)=> {handleCustomers(values)});
-Promise.all([roomsPromise]).then((values)=> {handleRooms(values)});
+Promise.all([bookingsPromise]).then((values) => { handleBookings(values) });
+Promise.all([customerPromise]).then((values) => { handleCustomers(values) });
+Promise.all([roomsPromise]).then((values) => { handleRooms(values) });
 let recievedBookings;
 let receivedCustomers;
 let receivedRooms;
-// var currentlyActiveCustomer = currentCustomer;
-async function listData (){
+let currentFilter;
+let currentDateSelection;
+
+const filterShowButton = document.querySelector('.dropbtn');
+
+async function listData() {
     // console.log((await bookingsPromise).bookings);
     // console.log((await customerPromise).customers);
     // console.log((await roomsPromise).rooms);
-
 }
-async function setDataVals (){
+async function setDataVals() {
     recievedBookings = (await bookingsPromise).bookings;
     receivedCustomers = (await customerPromise).customers;
     receivedRooms = (await roomsPromise).rooms;
@@ -20,45 +23,63 @@ async function setDataVals (){
     // console.log(receivedCustomers)
     // console.log(receivedRooms)
     return recievedBookings;
-    
+
 }
-async function currentBookings(){
-    var currentlyBooked =  (await bookingsPromise).bookings
-    // console.log(currentlyBooked);
-    await currentCustomer.id;
+async function currentBookings() {
+    var currentlyBooked = (await bookingsPromise).bookings
+    await currentCustomer;
     await receivedRooms;
     setDataVals();
-    var userBookings = currentlyBooked.filter((values)=>(values.userID === currentCustomer.id));
-    // console.log("for "+ currentCustomer.name + " with ID of "+currentCustomer.id);
+    var userBookings = currentlyBooked.filter((values) => (values.userID === currentCustomer.id));
     return userBookings
     // todo: Clean up, show in a table?
-    // console.log(await(recievedBookings.filter((booking) => {booking.userID === currentCustomer.id})));
 }
 
-async function hasSpent(userID){
+async function hasSpent(userID) {
     // await currentCustomer;
     await setDataVals();
 
     var totalSpent = 0;
-    var userBookedRooms = recievedBookings.filter((values)=>(values.userID === userID));
-    var timesPaidFor={};
+    var userBookedRooms = recievedBookings.filter((values) => (values.userID === userID));
+    var timesPaidFor = {};
     var roomsPaidFor = userBookedRooms.map((room) => room.roomNumber);
-    // console.log(roomsPaidFor)
-    for (const roomNum of roomsPaidFor){
-        timesPaidFor[roomNum] = timesPaidFor[roomNum] ? timesPaidFor[roomNum]+1 : 1;
+    for (const roomNum of roomsPaidFor) {
+        timesPaidFor[roomNum] = timesPaidFor[roomNum] ? timesPaidFor[roomNum] + 1 : 1;
     }
-    for(const roomNum of roomsPaidFor)
-    {
-        var roomCost = receivedRooms.find((room) => room.number ===roomNum).costPerNight;
-        totalSpent+= roomCost;
+    for (const roomNum of roomsPaidFor) {
+        var roomCost = receivedRooms.find((room) => room.number === roomNum).costPerNight;
+        totalSpent += roomCost;
     }
-    // console.log(timesPaidFor)
-    console.log(totalSpent)
     return totalSpent;
 }
 
+async function findFreeRooms(date, currentFilter = null) {
+    await setDataVals();
+    var bookedAlready = recievedBookings.filter((values) => (values.date === date)).map((room) => room.roomNumber);
+    var unbookedRooms = receivedRooms.filter((room) => !bookedAlready.includes(room.number))
 
-export{
+    //Todo: add text filtering to ensure date is entered correctly?//
+    if (currentFilter !== null) {
+        var tempForFilter = unbookedRooms.filter((room) => room.roomType === currentFilter);
+        unbookedRooms = tempForFilter;
+    }
+    return unbookedRooms;
+}
+
+async function showFilterRoomType() {
+    document.getElementById("myDropdown").classList.toggle("show");
+
+}
+
+function setFilter(){
+    //TODO: add function to set currentFilter based on button clicked, add event listeners for each kind of room type
+}
+
+filterShowButton.addEventListener('click', showFilterRoomType)
+
+
+
+export {
     receivedCustomers,
     receivedRooms,
     recievedBookings,
@@ -66,5 +87,11 @@ export{
     // totalSpent,
     currentBookings,
     hasSpent,
+    findFreeRooms,
+    showFilterRoomType,
+    currentFilter,
+    currentDateSelection,
+
+
 
 }
